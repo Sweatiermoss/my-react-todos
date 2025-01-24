8c2024ade49316d0e809b9740a2517e182933148
Sweatiermoss <mwiseneza65@gmail.com>
2025-01-24 14:43:35 -0600

    Tested components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import './App.css'
+import "./App.css";
+
+import { Button } from "react-bootstrap";
+import TodoList from "./components/TodoList";
+import { defaultTodos } from "./data";
 
 function App() {
+  console.log(defaultTodos);
+
   return (
-    <div>App</div>
-  )
+    <div className="container">
+      <h1>Todo App Example</h1>
+      <Button className="mt-2 mb-2">Add Todo</Button>
+      <TodoList todos={defaultTodos} />
+    </div>
+  );
 }
 
 export default App;
